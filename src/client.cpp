0502c5526158f31c49336fec6ca5e28429d6fd62
Barry Robinson <barry.w.robinson@gmail.com>
2025-12-17 12:10:10 +0000

    update cliet tpo rqate and pull toxes and display held tox

--- src/client.cpp
+++ src/client.cpp
@@ -1,80 +1,125 @@
 #include "client.h"
 
-#include <stdexcept>
-#include <cstring>
+#include <map>
+#include <vector>
 #include <iostream>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
-#include <arpa/inet.h>
-
-typedef struct __attribute__((packed)) tox_req_t {
-    uint8_t code;
-    uint64_t tox_id;
-} tox_req;
-
-#define htonll(x) ((((uint64_t)htonl(x)) << 32) + htonl((x) >> 32))
-
-int main() {
+int main()
+{
     Socket client("texttok.fzero.io", 4000);
 
-    // Ask for tox ID
-    uint8_t code = 1;
-    client.write_to_server(code);
+    constexpr size_t MAX_TOXES = 100;
+    constexpr int MENU_GET_TOX = 1;
+    constexpr int MENU_PRINT_TOXES = 2;
+    constexpr int UPVOTE_TOX = 3;
+    constexpr int DOWNVOTE_TOX = 4;
+    // constexpr int GET_RATING = 5;
+    constexpr int MENU_EXIT = 5;
 
-    // Read response
-    int bytes = client.read_response_from_server((void*) &code,sizeof(uint8_t));
+    // Tox ID to index in vector
+    std::map<uint32_t, size_t> toxes;
+    std::vector<Tox> tox_list;
 
-    // Get the values
-    if(code != 64) {
-        std::cerr << "Error response from server, ID: " << (int) code << std::endl;
-        return 1;
+    bool run = true;
+    while (run)
+    {
+        std::cout << "menu " << std::endl;
+        std::cout << "1. Get next Tox" << std::endl;
+        std::cout << "2. Print collected Toxes" << std::endl;
+        std::cout << "3. Upvote Tox" << std::endl;
+        std::cout << "4. Downvote Tox" << std::endl;
+        std::cout << "5. Exit" << std::endl;
+        std::cout << "Enter choice: ";
+
+        int choice;
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case MENU_GET_TOX:
+        {
+            Tox new_tox;
+            new_tox.get_latest(client);
+            uint32_t id = new_tox.getId();
+            if (toxes.find(id) == toxes.end())
+            {
+                toxes[id] = tox_list.size();
+                tox_list.push_back(new_tox);
+                std::cout << "Retrieved new tox ID: " << std::hex << id << std::dec << std::endl;
+            }
+            else
+            {
+                std::cout << "Tox ID: "  << std::hex << id << std::dec << " already retrieved." << std::endl;
+            }
+            break;
+        }
+        case MENU_PRINT_TOXES:
+        {
+            std::cout << "Collected Toxes: " << tox_list.size() << std::endl;
+            for (auto &tox : tox_list)
+            {
+                tox.print();
+            }
+            break;
+        }
+        case UPVOTE_TOX:
+        {
+            std::cout << "Please end the ID of the Tox to upvote: ";
+            uint32_t id;
+            std::cin >>  std::hex >> id >> std::dec;
+            auto tox = toxes.find(id);
+            if (tox != toxes.end())
+            {
+                tox_list[tox->second].vote(client, true);
+            }
+            else
+            {
+                std::cout << "Tox ID: "  << std::hex << id << std::dec << " not found in collected toxes." << std::endl;
+            }
+            break;
+        }
+        case DOWNVOTE_TOX:
+        {
+            std::cout << "Please enter the ID of the Tox to downvote: ";
+            uint32_t id;
+            std::cin >>  std::hex >> id >> std::dec;
+            auto tox = toxes.find(id);
+            if (tox != toxes.end())
+            {
+                tox_list[tox->second].vote(client, false);
+            }
+            else
+            {
+                std::cout << "Tox ID: "  << std::hex << id << std::dec << " not found in collected toxes." << std::endl;
+            }
+            break;
+        }
+        // case GET_RATING:
+        // {
+        //     std::cout << "Please enter the ID of the Tox to get rating: ";
+        //     uint32_t id;
+        //     std::cin >>  std::hex >> id >> std::dec;
+        //     auto tox = toxes.find(id);
+        //     if (tox != toxes.end())
+        //     {
+        //         tox_list[tox->second].get_rating(client);
+        //         std::cout << "Tox ID: " << std::hex << id << std::dec << " has rating: " << tox_list[tox->second].getRating() << std::endl;
+        //     }
+        //     else
+        //     {
+        //         std::cout << "Tox ID: "  << std::hex << id << std::dec << " not found in collected toxes." << std::endl;
+        //     }
+        //     break;
+        // }
+        case MENU_EXIT:
+        {
+            run = false;
+            break;
+        }
+        default:
+            std::cout << "Invalid choice. Please try again." << std::endl;
+        }
     }
-
-    uint32_t tox_id;
-
-    bytes = client.read_response_from_server((void*) &tox_id, sizeof(tox_id)); 
     
-    std::cout << "Response ID: " << (int) code << std::endl;
-    std::cout << "Tox ID: " << (int) tox_id << std::endl;
-    
-
-
-    std::cout << "Requesting message for Tox ID: " << (int) tox_id << std::endl;
-
-
-    // Ask for tox message
-    code = 2;
-    client.write_to_server(code);
-    client.write_to_server(tox_id);
-
-
-    uint32_t message_size;
-    bytes = client.read_response_from_server(&code, sizeof(code));
-    bytes = client.read_response_from_server(&message_size, sizeof(message_size));
-    if(code != 65) {
-        std::cerr << "Error response from server, code: " << (int)code << std::endl;
-        return 1;
-    }
-
-    std::cout << "Response ID: " << (int)code << std::endl;
-    std::cout << "Message size: " << (int)message_size << std::endl;
-
-    std::cout << "Message size to read: " <<(int) message_size << std::endl;
-
-    size_t size = static_cast<size_t>(message_size);
-    char * message = new char[size + 1];
-    message[message_size] = '\0';
-
-    client.read_response_from_server(message, message_size);
-    // memcpy(message, buff, size);
-
-    std::cout << "Message size: " << message_size << std::endl;
-    std::cout << "Message: " << std::string(message, message_size) << std::endl;
-
-    delete [] message;
-
     return 0;
 }
