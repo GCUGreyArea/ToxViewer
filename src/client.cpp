7b867dfd2f91e6f335c427447b4caa813dc1172b
Barry Robinson <barry.w.robinson@gmail.com>
2025-12-17 14:55:00 +0000

    add serialisation and updat em enu

--- src/client.cpp
+++ src/client.cpp
@@ -3,18 +3,54 @@
 #include <map>
 #include <vector>
 #include <iostream>
+#include <fstream>
+
+void serialise_all(std::vector<Tox> & toxlist,std::string file) {
+    std::ofstream of;
+    of.open(file);
+
+    size_t size = toxlist.size();
+
+    of.write((const char*) &size,sizeof(size));
+
+    for(auto & tox : toxlist) {
+        tox.serialise(of);
+    }
+}
+
+void deserialise_all(std::vector<Tox> & toxlist, std::map<uint32_t, size_t> & toxmap, std::string file) {
+    std::ifstream i;
+
+    i.open(file,std::ios::in);
+
+    // Get rid of the current toxes
+    toxlist.clear();
+    toxmap.clear();
+
+    size_t size;
+
+    i.read((char*)&size,sizeof(size));
+
+    for(size_t idx=0; idx<size;idx++) {
+        Tox t;
+        t.deserialise(i);
+        toxlist.push_back(t);
+        toxmap[t.getId()] = idx;
+    }
+}
 
 int main()
 {
     Socket client("texttok.fzero.io", 4000);
 
-    constexpr size_t MAX_TOXES = 100;
+    // constexpr size_t MAX_TOXES = 100;
     constexpr int MENU_GET_TOX = 1;
     constexpr int MENU_PRINT_TOXES = 2;
     constexpr int UPVOTE_TOX = 3;
     constexpr int DOWNVOTE_TOX = 4;
-    // constexpr int GET_RATING = 5;
-    constexpr int MENU_EXIT = 5;
+    constexpr int MENU_SAVE_TOX = 5;
+    constexpr int MENU_LOAD_TOX = 6;
+    constexpr int MENU_EXIT = 7;
 
     // Tox ID to index in vector
     std::map<uint32_t, size_t> toxes;
@@ -28,7 +64,9 @@ int main()
         std::cout << "2. Print collected Toxes" << std::endl;
         std::cout << "3. Upvote Tox" << std::endl;
         std::cout << "4. Downvote Tox" << std::endl;
-        std::cout << "5. Exit" << std::endl;
+        std::cout << "5. Save toxes to file" << std::endl;
+        std::cout << "6. Load toxes from file" << std::endl;
+        std::cout << "7. Exit" << std::endl;
         std::cout << "Enter choice: ";
 
         int choice;
@@ -94,28 +132,29 @@ int main()
             }
             break;
         }
-        // case GET_RATING:
-        // {
-        //     std::cout << "Please enter the ID of the Tox to get rating: ";
-        //     uint32_t id;
-        //     std::cin >>  std::hex >> id >> std::dec;
-        //     auto tox = toxes.find(id);
-        //     if (tox != toxes.end())
-        //     {
-        //         tox_list[tox->second].get_rating(client);
-        //         std::cout << "Tox ID: " << std::hex << id << std::dec << " has rating: " << tox_list[tox->second].getRating() << std::endl;
-        //     }
-        //     else
-        //     {
-        //         std::cout << "Tox ID: "  << std::hex << id << std::dec << " not found in collected toxes." << std::endl;
-        //     }
-        //     break;
-        // }
+        case MENU_SAVE_TOX: {
+            std::string fname;
+
+            std::cout << "Please supply a file name: ";
+            std::cin >> fname;
+            serialise_all(tox_list,fname);
+        }
+        break;
+
+        case MENU_LOAD_TOX: {
+            std::string fname;
+
+            std::cout << "Please supply a file name: ";
+            std::cin >> fname;
+            deserialise_all(tox_list,toxes,fname);
+        }
+        break;
         case MENU_EXIT:
         {
             run = false;
             break;
         }
+
         default:
             std::cout << "Invalid choice. Please try again." << std::endl;
         }
